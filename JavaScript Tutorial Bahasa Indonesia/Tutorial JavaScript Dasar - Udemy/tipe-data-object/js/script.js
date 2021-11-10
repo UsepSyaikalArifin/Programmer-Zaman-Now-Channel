1bd2e87b37ee2285315c08c466a39ba93399e36f
UsepSyaikalArifin <useparifinsyaikal@gmail.com>
2021-11-10 21:05:02 +0700

    Tipe Data Object

---
+++ JavaScript Tutorial Bahasa Indonesia/Tutorial JavaScript Dasar - Udemy/tipe-data-object/js/script.js
@@ -0,0 +1,18 @@
+const person = {}
+person[`nama`] = `Usep Syaikal Arifin`
+person[`alamat`] = `Indonesia`
+person[`umur`] = 30
+
+console.table(person)
+
+const orang = {
+    nama : `Muhammad Benjamin Partowidagdo`,
+    alamat : `Indonesia`,
+    umur : 30
+}
+
+console.table(orang)
+
+document.writeln(`<p>Nama : ${person.nama}</p>`)
+document.writeln(`<p>Alamat : ${person.alamat}</p>`)
+document.writeln(`<p>Umur : ${person.umur}</p>`)
