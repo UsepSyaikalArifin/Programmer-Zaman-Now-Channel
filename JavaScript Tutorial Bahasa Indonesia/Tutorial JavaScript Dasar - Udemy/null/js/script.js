75bb975a0a7ddaf7b2ce4a319702eaf4101fc1af
UsepSyaikalArifin <useparifinsyaikal@gmail.com>
2021-11-11 04:13:49 +0700

    Tipe Data Null

---
+++ JavaScript Tutorial Bahasa Indonesia/Tutorial JavaScript Dasar - Udemy/null/js/script.js
@@ -0,0 +1,9 @@
+let firstName = null
+
+if(firstName === null){
+    console.info(`null`)
+}else if(firstName === undefined){
+    console.info(`undefined`)
+}else{
+    console.info(`${firstName}`)
+}
