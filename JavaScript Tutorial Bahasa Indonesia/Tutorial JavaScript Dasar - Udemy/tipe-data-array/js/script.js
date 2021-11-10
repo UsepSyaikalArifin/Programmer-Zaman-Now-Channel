f2025ad566ae4599d436d643c5c4603034d94676
UsepSyaikalArifin <useparifinsyaikal@gmail.com>
2021-11-10 20:17:30 +0700

    Tipe Data Array

---
+++ JavaScript Tutorial Bahasa Indonesia/Tutorial JavaScript Dasar - Udemy/tipe-data-array/js/script.js
@@ -0,0 +1,15 @@
+// 1.
+let arrayKosong = []
+let arrayNama = [`Usep Syaikal Arifin`,`Muhammad Benjamin Partowidagdo`,`Muhammad Bunyamin Sarmowidagdo`]
+let arrayCampuran = [1, `Usep`, undefined, null, NaN]
+
+console.table(arrayKosong)
+console.table(arrayNama)
+console.table(arrayCampuran)
+
+// 2. Menambah Array
+const names = []
+names.push(`Usep Syaikal Arifin`)
+names.push(`Muhamaad Benjamin Partowidagdo`, `Muhammad Bunyamin Sarmowidagdo`)
+
+console.table(names)
