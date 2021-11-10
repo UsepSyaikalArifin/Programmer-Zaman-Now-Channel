049c51b673625839f159344d4ee026e243ebc158
UsepSyaikalArifin <useparifinsyaikal@gmail.com>
2021-11-10 17:34:02 +0700

    Kursus JavaScript Dasar - Udemy - Operator Matematika - Perbandingan

---
+++ JavaScript Tutorial Bahasa Indonesia/Tutorial JavaScript Dasar - Udemy/operator-perbandingan/js/script.js
@@ -0,0 +1,8 @@
+let result = 5 == `5`
+document.writeln(`<p>5 == '5' = ${result}</p>`)
+result = 5 === `5`
+document.writeln(`<p>5 === '5' = ${result}</p>`)
+result = 5 == 5
+document.writeln(`<p>5 == 5 = ${result}</p>`)
+result = 5 === 5
+document.writeln(`<p>5 === 5 = ${result}</p>`)
