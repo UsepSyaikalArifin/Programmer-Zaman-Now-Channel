5f41be94f034feec2053599cdcbc0a6de0cfbad6
UsepSyaikalArifin <useparifinsyaikal@gmail.com>
2021-11-10 18:50:20 +0700

    Kursus JavaScript Dasar - Udemy - Operator Matematika - Operator Logika

--- JavaScript Tutorial Bahasa Indonesia/Tutorial JavaScript Dasar - Udemy/operator-logika/js/script.js
+++ JavaScript Tutorial Bahasa Indonesia/Tutorial JavaScript Dasar - Udemy/operator-logika/js/script.js
@@ -1,8 +1,11 @@
-const nilaiUjian = 70
+// 1. Operator Perbandingan &&
+const nilaiUjian = 90
 const nilaiAbsensi = 70
 
 const lulusUjian = nilaiUjian > 75
 const lulusAbsensi = nilaiAbsensi > 75
 
 const lulus = lulusUjian && lulusAbsensi
+const lulus2 = lulusUjian || lulusAbsensi
 document.writeln(`<p> ${lulus} </p>`)
+document.writeln(`<p> ${lulus2} </p>`)
