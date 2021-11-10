03a1a2d82ac364d49949ae485e7573e75d718e5e
UsepSyaikalArifin <useparifinsyaikal@gmail.com>
2021-11-11 06:27:00 +0700

    Operator In Modify

--- JavaScript Tutorial Bahasa Indonesia/Tutorial JavaScript Dasar - Udemy/operator-in/js/script.js
+++ JavaScript Tutorial Bahasa Indonesia/Tutorial JavaScript Dasar - Udemy/operator-in/js/script.js
@@ -1,7 +1,22 @@
+// 1. Bentuk 1
 const person = {
     firstName : `Usep`,
+    middleName : undefined,
     lastName : `Syaikal`
 }
 
 const cekProperty = `firstName` in person
+const cekPropertyund = `lastName` in person
 console.info(cekProperty)
+console.info(cekPropertyund)
+
+if(cekProperty === true){
+    console.info(`Hello ${person.firstName}`)
+}else{
+    console.info(`Hello`)
+}
+
+// 2.
+const names = [null, `Usep`, null]
+const cekIndex = 0 in names
+console.info(cekIndex)
