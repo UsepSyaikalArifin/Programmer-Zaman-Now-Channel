4f5d59471a28e43fde3c21c74fb40bac5f0da136
UsepSyaikalArifin <useparifinsyaikal@gmail.com>
2021-11-10 20:29:48 +0700

    Tipe Data Array Modify

--- JavaScript Tutorial Bahasa Indonesia/Tutorial JavaScript Dasar - Udemy/tipe-data-array/js/script.js
+++ JavaScript Tutorial Bahasa Indonesia/Tutorial JavaScript Dasar - Udemy/tipe-data-array/js/script.js
@@ -11,5 +11,22 @@ console.table(arrayCampuran)
 const names = []
 names.push(`Usep Syaikal Arifin`)
 names.push(`Muhamaad Benjamin Partowidagdo`, `Muhammad Bunyamin Sarmowidagdo`)
+names.push(`Joko`, `Widodo`, `Budi`)
 
 console.table(names)
+console.info(names[0])
+console.info(names[1])
+console.info(names[2])
+
+names[0] = `Programmer`
+console.table(names)
+
+delete names[3]
+console.table(names)
+names.push(`Joko Lagi`)
+names[3] = "Budi Lagi"
+console.table(names)
+names.push(1, 2, 3, 4, 5)
+names.push([1, 2, "1",  `2`, 3, [1, 2, 3, 4, [`Usep`,`Syaikal`]]])
+console.table(names)
+
