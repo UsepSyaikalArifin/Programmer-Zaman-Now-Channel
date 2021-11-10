741b5e784580bd68764bc7f1e8f3758383ea890f
UsepSyaikalArifin <useparifinsyaikal@gmail.com>
2021-11-10 21:16:46 +0700

    If Ekspression

---
+++ JavaScript Tutorial Bahasa Indonesia/Tutorial JavaScript Dasar - Udemy/if-ekspression/js/script.js
@@ -0,0 +1,22 @@
+// 1. Bentuk ke - 1
+const examValue = 70
+
+if(examValue > 69){
+    document.writeln(`<p>Good Job</p>`)
+}
+
+// 2. 
+if(examValue > 80){
+    document.writeln(`<p>Good Job</p>`)
+}else{
+    document.writeln(`<p>Try Again Later</p>`)
+}
+
+// 3.
+if(examValue > 80){
+    document.writeln(`<p>Good Job</p>`)
+}else if(examValue > 60 && examValue <=80){
+    document.writeln(`<p>Not Bad</p>`)
+}else{
+    document.writeln(`<p>Try Again Later</p>`)
+}
