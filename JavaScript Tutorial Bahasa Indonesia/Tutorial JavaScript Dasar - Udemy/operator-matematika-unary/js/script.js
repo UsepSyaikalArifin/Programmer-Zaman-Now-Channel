b7679ff4d5484cf706091f5d8dd8a91f505b817c
UsepSyaikalArifin <useparifinsyaikal@gmail.com>
2021-11-10 17:29:09 +0700

    Kursus JavaScript Dasar - Udemy - Operator Matematika - Unary

---
+++ JavaScript Tutorial Bahasa Indonesia/Tutorial JavaScript Dasar - Udemy/operator-matematika-unary/js/script.js
@@ -0,0 +1,16 @@
+let result = +1
+document.writeln(`<p>${result}</p>`)
+result--
+document.writeln(`<p>${result}</p>`)
+result--
+document.writeln(`<p>${result}</p>`)
+result--
+document.writeln(`<p>${result}</p>`)
+result--
+document.writeln(`<p>${result}</p>`)
+result--
+document.writeln(`<p>${result}</p>`)
+result++
+document.writeln(`<p>${result}</p>`)
+result = -result
+document.writeln(`<p>${result}</p>`)
