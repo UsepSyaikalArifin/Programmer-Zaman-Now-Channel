b65dd6d59b3580c2ed71b59f1bc5f19f78becd9b
UsepSyaikalArifin <useparifinsyaikal@gmail.com>
2021-11-11 04:49:06 +0700

    Switch Ekspression

---
+++ JavaScript Tutorial Bahasa Indonesia/Tutorial JavaScript Dasar - Udemy/switch-ekspression/js/script.js
@@ -0,0 +1,15 @@
+let nilai = `C`
+switch(nilai){
+    case `A`:
+        console.info(`Wow anda lulus dengan baik`)
+        break
+    case `B`:
+    case `C`:
+        console.info(`Anda lulus`)
+        break
+    case `D`:
+        console.info(`Anda tidak lulus`)
+        break
+    default:
+        console.info(`Mungkin anda salah jurusan`)
+}
