ab7f1ec6c76a4e6ed3bab625b43247773dd77127
UsepSyaikalArifin <useparifinsyaikal@gmail.com>
2021-11-10 17:01:07 +0700

    Kursus JavaScript Dasar - Udemy - Tipe Data Variable

---
+++ JavaScript Tutorial Bahasa Indonesia/Tutorial JavaScript Dasar - Udemy/variable/js/script.js
@@ -0,0 +1,37 @@
+// 1.
+document.writeln(`<p>Usep Syaikal Arifin</p>`)
+document.writeln(`<p>Usep Syaikal Arifin</p>`)
+document.writeln(`<p>Usep Syaikal Arifin</p>`)
+document.writeln(`<p>Usep Syaikal Arifin</p>`)
+document.writeln(`<p>Usep Syaikal Arifin</p>`)
+document.writeln(`<p>Usep Syaikal Arifin</p>`)
+document.writeln(`<p>Usep Syaikal Arifin</p>`)
+document.writeln(`<p>Usep Syaikal Arifin</p>`)
+
+// 2. Menggunakan Variable
+let fullName
+let firstName
+let middleName
+let lastName
+
+fullName = `Usep Syaikal Arifin`
+firstName = `Usep`
+middleName = `Syaikal`
+lastName = `Arifin`
+lastName = `Widagdo`
+lastName = `Partowidagdo`
+
+// 3. Membuat variable langsung dengan datanya
+let fName = `Muhammad Benjamin Partowidagdo`
+let lName = `Arifin`
+let fName2 = `Usep` + `Syaikal` + `Arifin`
+document.writeln(`<p>${fName2}</p>`)
+fName2 = `Widagdo`
+document.writeln(`<p>${fName2}</p>`)
+
+// 4. Penggunaan variabel berulang
+document.writeln(`<p>${fName}</p>`)
+document.writeln(`<p>${fName}</p>`)
+document.writeln(`<p>${fName}</p>`)
+document.writeln(`<p>${fName}</p>`)
+document.writeln(`<p>${fName}</p>`)
