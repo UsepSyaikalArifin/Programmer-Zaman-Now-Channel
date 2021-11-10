625ad7af9337514f5bbf15af90e42b5ebae359c8
UsepSyaikalArifin <useparifinsyaikal@gmail.com>
2021-11-10 16:03:41 +0700

    Kursus JavaScript Dasar - Udemy - Tipe Data String

---
+++ JavaScript Tutorial Bahasa Indonesia/Tutorial JavaScript Dasar - Udemy/tipe-data-string/js/script.js
@@ -0,0 +1,17 @@
+// 1. Tipe Data String
+document.writeln(`<p>Usep Syaikal Arifin</p>`)
+document.writeln(`<p>ASN || Web Developer || Data Scientist</p>`)
+
+// 2. Menggabungkan Tipe Data String
+document.writeln("Nama : " + "Usep Syaikal Arifin")
+document.writeln("</br>")
+document.writeln("ASN " + "|| " + "Web Developer" + "|| " + "Data Scientist")
+document.writeln("</br>")
+
+// 3. Escape Sequence (Karakter Khusus)
+document.writeln("<textarea cols='100' rows='10'>")
+document.write("Usep \nSyaikal \nArifin\n")
+document.write("\\Home \\Khanedy\n")
+document.write("\"Programmer Zaman Now\"\n")
+document.write("\'Web Developer\'\n")
+document.writeln("</textarea>")
