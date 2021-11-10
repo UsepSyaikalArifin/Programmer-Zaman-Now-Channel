fdf08fc73ef03b1f6fe6489a3b8f8d6e60d15400
UsepSyaikalArifin <useparifinsyaikal@gmail.com>
2021-11-10 15:20:37 +0700

    Kursus JavaScript Dasar - Udemy - Tipe Data Number

---
+++ JavaScript Tutorial Bahasa Indonesia/Tutorial JavaScript Dasar - Udemy/tipe-data-number/js/script.js
@@ -0,0 +1,10 @@
+// 1. Tipe data number basis 10
+document.writeln(`<p>${100}</p>`)
+document.writeln(`<p>${100.123}</p>`)
+
+// 2. Tipe data number basis 2 (binary)
+document.writeln(`<p>${0b10101}</p>`)
+// 2. Tipe data number basis  (oktal)
+document.writeln(`<p>${0b10101}</p>`)
+// 2. Tipe data number basis  (hexadecimal)
+document.writeln(`<p>${0o10101}</p>`)
