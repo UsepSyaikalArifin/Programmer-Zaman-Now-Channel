fedbe3ac34c558151a311235e1b5688881ce5622
UsepSyaikalArifin <useparifinsyaikal@gmail.com>
2021-11-11 14:23:01 +0700

    Rest Parameter Modify

--- JavaScript Tutorial Bahasa Indonesia/Tutorial JavaScript Dasar - Udemy/function-rest-parameter/js/script.js
+++ JavaScript Tutorial Bahasa Indonesia/Tutorial JavaScript Dasar - Udemy/function-rest-parameter/js/script.js
@@ -6,4 +6,7 @@ function sum(name, ...data) {
     document.writeln(`<p>${name} : ${total}</p>`)
 }
 sum(`Apel`, 1, 2, 3, 4, 5, 6)
-sum(`Banan`, 10, 20, 30, 40, 50, 60)
+sum(`Banana`, 10, 20, 30, 40, 50, 60)
+
+const values = [10, 20, 30, 40, 50, 60]
+sum(`Banana`, ...values)
