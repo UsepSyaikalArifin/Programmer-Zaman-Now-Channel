ee851d08f1373c81e6731d9a1e6a9bc58c5c3824
UsepSyaikalArifin <useparifinsyaikal@gmail.com>
2021-11-11 10:06:29 +0700

    For Loop

---
+++ JavaScript Tutorial Bahasa Indonesia/Tutorial JavaScript Dasar - Udemy/for-loop/js/script.js
@@ -0,0 +1,16 @@
+// 1. Perulangan tidak berhenti
+// for(;;){
+//     alert(`Perulangan tidak berhenti`)
+// }
+
+// 2. Bentuk ke - 2
+let counter = 1
+for(;counter <= 10;){
+    document.writeln(`<p>Perulangan ke - ${counter}</p>`)
+    counter++
+}
+
+// 3. Bentuk ke - 3
+for(let i = 1; i <= 10; i++){
+    document.writeln(`<p>Perulangan ke - ${i}</p>`)
+}
