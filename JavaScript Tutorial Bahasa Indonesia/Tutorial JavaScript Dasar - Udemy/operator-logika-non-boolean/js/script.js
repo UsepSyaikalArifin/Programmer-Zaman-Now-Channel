44ea819af1432805f30bbf927d368203628ebc55
UsepSyaikalArifin <useparifinsyaikal@gmail.com>
2021-11-11 09:39:19 +0700

    Operator Logika Non Boolean Modify

--- JavaScript Tutorial Bahasa Indonesia/Tutorial JavaScript Dasar - Udemy/operator-logika-non-boolean/js/script.js
+++ JavaScript Tutorial Bahasa Indonesia/Tutorial JavaScript Dasar - Udemy/operator-logika-non-boolean/js/script.js
@@ -4,3 +4,18 @@ console.info(undefined || null)
 console.info(0 || NaN)
 console.info(null || undefined)
 console.info(NaN || 0)
+
+console.info(`Hello` && ``)
+console.info(`` && `Hello`)
+console.info(undefined && null)
+console.info(0 && NaN)
+console.info(null && undefined)
+console.info(NaN && 0)
+
+const person = {
+    firstName : `Usep`,
+    middleName :  `Syaikal`,
+    lastName :  `Arifin`
+}
+
+const name = person.firstName || person.lastName
