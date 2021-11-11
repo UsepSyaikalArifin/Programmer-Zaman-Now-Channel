0f83855159c703191e16cf2c5b5db6290afd448c
UsepSyaikalArifin <useparifinsyaikal@gmail.com>
2021-11-11 11:55:42 +0700

    For Of

---
+++ JavaScript Tutorial Bahasa Indonesia/Tutorial JavaScript Dasar - Udemy/For-of/js/script.js
@@ -0,0 +1,10 @@
+const fullName = `Usep Syaikal Arifin`
+const names = [`Usep`, `Syaikal`, `Arifin`]
+
+for(const name of names){
+    document.writeln(`<p>${name}</p>`)
+}
+
+for(const huruf of fullName){
+    document.writeln(`<p>${huruf}</p>`)
+}
