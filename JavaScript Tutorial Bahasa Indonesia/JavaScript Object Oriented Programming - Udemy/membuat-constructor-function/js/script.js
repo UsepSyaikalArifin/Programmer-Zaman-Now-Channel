3d9c864ea468c8e1f5f1bcae582bf446f3aa12d3
UsepSyaikalArifin <useparifinsyaikal@gmail.com>
2021-11-12 05:59:06 +0700

    Properti didalam Constructor Function

--- JavaScript Tutorial Bahasa Indonesia/JavaScript Object Oriented Programming - Udemy/membuat-constructor-function/js/script.js
+++ JavaScript Tutorial Bahasa Indonesia/JavaScript Object Oriented Programming - Udemy/membuat-constructor-function/js/script.js
@@ -3,7 +3,15 @@ const usep = {
     firstName : `Usep Syaikal`,
 
 }
-
 const benjamin = {
     namaDepan : `Muhammad Benjamin`,
 }
+
+// 2. Implementasi OOP di JS lama
+// Constructor function
+function Person(){
+
+}
+
+const classPerson_1 = new Person()
+const classPerson_2 = new Person()
---
+++ JavaScript Tutorial Bahasa Indonesia/JavaScript Object Oriented Programming - Udemy/property-constructor-function/js/script.js
@@ -0,0 +1,15 @@
+// 1. Consttructor function
+function Person(){
+    this.firstName = ``,
+    this.lastName = ``
+}
+
+const usep = new Person()
+usep.firstName = `Usep Syaikal`
+usep.lastName = `Arifin`
+const asep = new Person()
+asep.firstName = `Asep faisal`
+asep.lastName = `Arifin`
+
+console.info(usep)
+console.info(asep)
