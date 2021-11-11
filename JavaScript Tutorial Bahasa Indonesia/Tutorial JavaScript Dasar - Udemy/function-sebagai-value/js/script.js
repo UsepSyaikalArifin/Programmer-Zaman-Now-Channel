f88f7d4b3efeb5f1f421fe693ccd0854cb1f37e3
UsepSyaikalArifin <useparifinsyaikal@gmail.com>
2021-11-11 14:54:43 +0700

    Function sebagai value -> Function ekspression

---
+++ JavaScript Tutorial Bahasa Indonesia/Tutorial JavaScript Dasar - Udemy/function-sebagai-value/js/script.js
@@ -0,0 +1,14 @@
+// 1. Function ekspression bentuk ke - 1
+function sayHello(nama) {
+    document.writeln(`<p>Hello ${nama}</p>`)
+}
+
+let say = sayHello
+sayHello(`Usep Syaikal Arifin`)
+say(`Usep`)
+
+// 2. Function ekspression bentuk ke - 2
+function giveMeName(nama, callback) {
+    callback(nama)
+}
+giveMeName(`Jagoan`,say)
