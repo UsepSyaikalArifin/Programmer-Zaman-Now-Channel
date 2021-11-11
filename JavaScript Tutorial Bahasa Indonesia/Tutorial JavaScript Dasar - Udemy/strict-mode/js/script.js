0ea89ec70c62c34850122529d58d3bb204c69446
UsepSyaikalArifin <useparifinsyaikal@gmail.com>
2021-11-11 21:33:50 +0700

    Strict Mode Modify

--- JavaScript Tutorial Bahasa Indonesia/Tutorial JavaScript Dasar - Udemy/strict-mode/js/script.js
+++ JavaScript Tutorial Bahasa Indonesia/Tutorial JavaScript Dasar - Udemy/strict-mode/js/script.js
@@ -1,1 +1,11 @@
-`use strict`
+function useStrictMode(){
+    "use strict"
+    const person = {
+        firstName : `Usep`
+    }
+    with(person){
+        console.info(firstName)
+    }
+}
+
+useStrictMode()
