8cdae01a6f73fb673fc4862bf6f16970726b27dd
UsepSyaikalArifin <useparifinsyaikal@gmail.com>
2021-11-11 13:06:10 +0700

    Function dasar

--- JavaScript Tutorial Bahasa Indonesia/Tutorial JavaScript Dasar - Udemy/function/js/script.js
+++ JavaScript Tutorial Bahasa Indonesia/Tutorial JavaScript Dasar - Udemy/function/js/script.js
@@ -1,11 +1,13 @@
+// 1. Bentuk ke - 1
 function sayHello() {
     document.writeln(`<p>Hello World</p>`)
 }
 
-
+// variable berisi function
 const hello_1 = sayHello
 const hello_2 = sayHello()
 
+// pemanggilan function
 sayHello()
 hello_2
 hello_1()
