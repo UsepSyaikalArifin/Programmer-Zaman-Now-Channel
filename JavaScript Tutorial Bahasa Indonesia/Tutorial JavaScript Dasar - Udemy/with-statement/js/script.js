0ad1c8604a0e4298c2c4404f48c655a212516553
UsepSyaikalArifin <useparifinsyaikal@gmail.com>
2021-11-11 12:18:33 +0700

    With Statement modify

--- JavaScript Tutorial Bahasa Indonesia/Tutorial JavaScript Dasar - Udemy/with-statement/js/script.js
+++ JavaScript Tutorial Bahasa Indonesia/Tutorial JavaScript Dasar - Udemy/with-statement/js/script.js
@@ -8,6 +8,9 @@ for(const properti in person){
     document.writeln(`<p>Properti ${properti} = ${person[properti]}</p>`)
 }
 
+const firstName = `Eko`
+const lastName = `Nugraha`
+
 with(person){
         document.writeln(`<p>${firstName}</p>`)
         document.writeln(`<p>${middleName}</p>`)
