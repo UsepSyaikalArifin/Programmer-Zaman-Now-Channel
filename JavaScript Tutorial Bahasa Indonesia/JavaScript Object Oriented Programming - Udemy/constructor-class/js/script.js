0a6bdf56039bb32419588c57260c39bf5a12bcf9
UsepSyaikalArifin <useparifinsyaikal@gmail.com>
2021-11-12 14:17:01 +0700

    Constructor Class

---
+++ JavaScript Tutorial Bahasa Indonesia/JavaScript Object Oriented Programming - Udemy/constructor-class/js/script.js
@@ -0,0 +1,9 @@
+class Person{
+    constructor(name){
+        console.info(`Membuat person ${name}`)
+    }
+}
+
+const usep = new Person(`Usep Syaikal Arifin`)
+const budi = new Person(`Budi Gunawan`)
+console.info(usep)
