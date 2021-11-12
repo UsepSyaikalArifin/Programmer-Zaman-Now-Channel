2c9609f9621201c9221547e2e8d03bfe904fc1d2
UsepSyaikalArifin <useparifinsyaikal@gmail.com>
2021-11-13 04:06:42 +0700

    Getter & Setter

---
+++ JavaScript Tutorial Bahasa Indonesia/JavaScript Object Oriented Programming - Udemy/getter-seeter/js/script.js
@@ -0,0 +1,23 @@
+// 1.
+class Person{
+    constructor(firstName, lastName){
+        this.firstName = firstName
+        this.lastName = lastName
+    }
+
+    get fullName(){
+        return `${this.firstName} ${this.lastName}`
+    }
+
+    set fullName(value){
+        const result = value.split(` `)
+        this.firstName = result[0]
+        this.lastName = result[1]
+    }
+}
+
+const usep = new Person(`Usep`, `Syaikal`)
+console.info(usep)
+
+usep.fullName =  `Muhammad Benjamin`
+document.writeln(`<p>${usep.fullName}</p>`)
