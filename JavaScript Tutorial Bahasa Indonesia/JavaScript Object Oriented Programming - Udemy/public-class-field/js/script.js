23b039434e89ed3da6a4a2e729ac72f9e2ea416e
UsepSyaikalArifin <useparifinsyaikal@gmail.com>
2021-11-13 04:30:19 +0700

    Public Class Field

---
+++ JavaScript Tutorial Bahasa Indonesia/JavaScript Object Oriented Programming - Udemy/public-class-field/js/script.js
@@ -0,0 +1,16 @@
+class Customer{
+    firstName
+    lastName
+    balance = 0
+    constructor(firstName, lastName){
+        this.firstName = firstName
+        this.lastName = lastName
+    }
+    sayHello(){
+
+    }
+}
+
+
+const usep = new Customer(`Usep`, `Syaikal`)
+console.info(usep)
