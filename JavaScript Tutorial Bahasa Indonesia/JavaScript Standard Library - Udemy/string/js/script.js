18afc50e917862453d9910beef65952d1019d804
UsepSyaikalArifin <useparifinsyaikal@gmail.com>
2021-11-14 17:59:40 +0700

    String

---
+++ JavaScript Tutorial Bahasa Indonesia/JavaScript Standard Library - Udemy/string/js/script.js
@@ -0,0 +1,10 @@
+const name = `Usep Syaikal Arifin`
+const input = `    input data  `
+
+console.info(name.length)
+console.info(name.toLowerCase())
+console.info(name.toUpperCase())
+console.info(name.split(` `))
+console.info(name)
+console.info(input)
+console.info(input.trim())
