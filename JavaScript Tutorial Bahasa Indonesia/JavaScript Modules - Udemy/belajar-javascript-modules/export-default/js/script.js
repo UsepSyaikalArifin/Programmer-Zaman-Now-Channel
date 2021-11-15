ad357a3c4ddea02d44d2c60ac37977a5d5362505
UsepSyaikalArifin <useparifinsyaikal@gmail.com>
2021-11-16 05:17:04 +0700

    Modify Export & Name

--- JavaScript Tutorial Bahasa Indonesia/JavaScript Modules - Udemy/belajar-javascript-modules/export-default/js/script.js
+++ JavaScript Tutorial Bahasa Indonesia/JavaScript Modules - Udemy/belajar-javascript-modules/export-default/js/script.js
@@ -1,6 +1,7 @@
 import sayHelloMjs from "./say-hello.mjs";
 import defaultVariable from "./default-variable.mjs"
 import defaultClass from "./default-class.mjs"
+import authorDefault, {title, content} from "./default-and-name.mjs"
 
 
 sayHelloMjs(`Usep`)
@@ -8,3 +9,7 @@ console.info(defaultVariable)
 
 const usep = new defaultClass(`Jagoan`)
 usep.sayHi(`Pecundang`)
+
+console.info(authorDefault)
+console.info(title)
+console.info(content)
