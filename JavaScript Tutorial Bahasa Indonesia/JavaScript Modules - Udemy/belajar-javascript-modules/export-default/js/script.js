9a6a0bfc4a53d01a0bad272a013d3c6e6a47dad6
UsepSyaikalArifin <useparifinsyaikal@gmail.com>
2021-11-16 05:09:31 +0700

    Default -> Class

---
+++ JavaScript Tutorial Bahasa Indonesia/JavaScript Modules - Udemy/belajar-javascript-modules/export-default/js/script.js
@@ -0,0 +1,10 @@
+import sayHelloMjs from "./say-hello.mjs";
+import defaultVariable from "./default-variable.mjs"
+import defaultClass from "./default-class.mjs"
+
+
+sayHelloMjs(`Usep`)
+console.info(defaultVariable)
+
+const usep = new defaultClass(`Jagoan`)
+usep.sayHi(`Pecundang`)
