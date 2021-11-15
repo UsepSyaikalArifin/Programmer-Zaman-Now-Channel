fe7574ec332c34aade6ce0c2f2cb2e42f1317acd
UsepSyaikalArifin <useparifinsyaikal@gmail.com>
2021-11-16 04:47:34 +0700

    Alias

---
+++ JavaScript Tutorial Bahasa Indonesia/JavaScript Modules - Udemy/belajar-javascript-modules/alias/js/alias_export.mjs
@@ -0,0 +1,8 @@
+const company = `Programmer Python Handal`
+function sum(first, second){
+    return first + second
+}
+class Company{
+}
+
+export {company as perusahaan, sum as total, Company as Perusahaan}
---
+++ JavaScript Tutorial Bahasa Indonesia/JavaScript Modules - Udemy/belajar-javascript-modules/alias/js/script.js
@@ -0,0 +1,6 @@
+import { perusahaan, total, Perusahaan } from "./alias_export.mjs";
+
+console.info(perusahaan)
+console.info(total(1,2))
+const a = new Perusahaan()
+console.info(a)
