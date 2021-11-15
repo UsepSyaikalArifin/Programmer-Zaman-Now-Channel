10850f4db6dbfb85ab94589e3ce543806b215633
UsepSyaikalArifin <useparifinsyaikal@gmail.com>
2021-11-16 05:17:53 +0700

    Modify Export & Name

---
+++ JavaScript Tutorial Bahasa Indonesia/JavaScript Modules - Udemy/belajar-javascript-modules/export-default/js/default-and-name.mjs
@@ -0,0 +1,6 @@
+export const title = `Programmer ReactJs Handal`
+const content = `Content Belajar JavaScript Module`
+const author = `Prof Usep Syaikal Arifin`
+
+export {content}
+export default author
