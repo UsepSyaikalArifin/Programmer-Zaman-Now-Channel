1fd13f4106647f9d288f68382d08b70fc6605f7a
UsepSyaikalArifin <useparifinsyaikal@gmail.com>
2021-11-15 19:22:13 +0700

    Membuat Module

---
+++ JavaScript Tutorial Bahasa Indonesia/JavaScript Modules - Udemy/belajar-javascript-modules/membuat-module/js/say.mjs
@@ -0,0 +1,8 @@
+export function sayHello(name){
+    console.info(`Hello ${name}`)
+}
+export function sayGoodBye(name){
+    console.info(`GoodBYe ${name}`)
+}
+
+const name = `Prof Dr.Eng Usep Syaikal Arifin, M.Eng, M.Sc, Ph.D`
---
+++ JavaScript Tutorial Bahasa Indonesia/JavaScript Modules - Udemy/belajar-javascript-modules/membuat-module/js/script.js
@@ -0,0 +1,5 @@
+import {sayHello, sayGoodBye, name} from "./say.mjs"
+
+sayHello(`Prof Dr.Eng Usep Syaikal Arifin, M.Eng, M.Sc, Ph.D`)
+sayGoodBye(`Prof Dr.Eng Usep Syaikal Arifin, M.Eng, M.Sc, Ph.D`)
+console.info(name)
