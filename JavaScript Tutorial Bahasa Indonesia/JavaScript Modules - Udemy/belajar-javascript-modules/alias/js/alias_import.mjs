8a146c8320d2e617372e7761a76432a13ad50487
UsepSyaikalArifin <useparifinsyaikal@gmail.com>
2021-11-16 04:53:05 +0700

    Modify Alias

---
+++ JavaScript Tutorial Bahasa Indonesia/JavaScript Modules - Udemy/belajar-javascript-modules/alias/js/alias_import.mjs
@@ -0,0 +1,9 @@
+const company =  `Programmer ReactJS Handal`
+function sum(first, second){
+    return first + second
+}
+class Company{
+
+}
+
+export {company, sum, Company}
--- JavaScript Tutorial Bahasa Indonesia/JavaScript Modules - Udemy/belajar-javascript-modules/alias/js/script.js
+++ JavaScript Tutorial Bahasa Indonesia/JavaScript Modules - Udemy/belajar-javascript-modules/alias/js/script.js
@@ -1,6 +1,12 @@
 import { perusahaan, total, Perusahaan } from "./alias_export.mjs";
+import {company as perusahaan2, sum as total2, Company as Perusahaan2} from "./alias_import.mjs"
 
 console.info(perusahaan)
 console.info(total(1,2))
 const a = new Perusahaan()
 console.info(a)
+
+console.info(perusahaan2)
+console.info(total2(2,2))
+const b = new Perusahaan2()
+console.info(b)
